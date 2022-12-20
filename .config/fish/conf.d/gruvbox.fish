a054d4758e1f1a541a05c58f008e6e9911e8d2cd
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-19 23:18:31 -0300

    feat(fish): add theme files inside conf.d

---
+++ .config/fish/conf.d/gruvbox.fish
@@ -0,0 +1,29 @@
+# set colorscheme gruvbox
+set -U fish_color_normal "#282828"
+set -U fish_color_command "#ebdbb2"
+set -U fish_color_quote "#ebdbb2"
+set -U fish_color_redirection "#ebdbb2"
+set -U fish_color_end "#ebdbb2"
+set -U fish_color_error "#fb4934"
+set -U fish_color_escape "#fb4934"
+set -U fish_color_cwd "#83a598"
+set -U fish_color_cwd_root "#fb4934"
+set -U fish_color_match "#fb4934"
+set -U fish_color_selection "#282828"
+set -U fish_color_search_match "#83a598"
+set -U fish_color_operator "#83a598"
+set -U fish_color_param "#83a598"
+set -U fish_color_comment "#928374"
+set -U fish_color_history_current "#ebdbb2"
+set -U fish_color_host "#83a598"
+set -U fish_color_autosuggestion "#83a598"
+set -U fish_color_valid_path "#b8bb26"
+set -U fish_color_user brgreen
+set -U fish_color_cancel -r
+
+# hydro prompt settings with gruvbox dark 
+set hydro_color_prompt "#ebdbb2"
+set hydro_color_pwd "#83a598"
+set hydro_color_git "#83a598"
+set hydro_color_error "#fb4934"
+set hydro_color_duration "#b8bb26"
