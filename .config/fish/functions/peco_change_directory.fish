9e3f0081c7ec78c7f2889d58be7e68c3ade29b1b
datsfilipe <datsfilipe.pro@proton.me>
2023-02-11 22:57:14 -0300

    feat(fish): fix peco_change_directory functions errors adding checks
    now the function is making a list of directories and avoiding empty directories and non-existent
    directories.

--- .config/fish/functions/peco_change_directory.fish
+++ .config/fish/functions/peco_change_directory.fish
@@ -1,3 +1,4 @@
+# use peco to list directories
 function _peco_change_directory
   if [ (count $argv) ]
     peco --layout=bottom-up --query "$argv "|perl -pe 's/([ ()])/\\\\$1/g'|read foo
@@ -14,12 +15,45 @@ function _peco_change_directory
 end
 
 function peco_change_directory
-  begin
-    echo $HOME/.config
-    ghq list -p
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/www/*/* |grep -v \.git
-    ls -ad $HOME/www/personal/posts
-    ls -d /run/media/*/*
-  end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
+  # adding simple dirs to the list
+  set dir_list (
+    realpath $HOME/.config
+    realpath $HOME/www/personal/posts
+    realpath $HOME/.test-config/nvim
+  )
+
+  # adding ghq list
+  for dir in (ghq list -p)
+    set dir_list $dir_list $dir
+  end
+
+  # adding git dirs
+  for dir in (ls -ad */ | grep -v \.git)
+    set dir_list $dir_list $PWD/$dir
+  end
+
+  # adding home dirs inside www (my projects folder)
+  for dir in (ls -ad (realpath $HOME/www/*/*) | grep -v \.git)
+    set dir_list $dir_list $dir
+  end
+
+  # adding mounted drivers dirs
+  for dir in (ls -d /run/media/*/*)
+    set dir_list $dir_list $dir
+  end
+
+  # filtering dirs by existence and non-emptiness
+  set dir_list_filtered ( )
+  for dir in $dir_list
+    if test -d $dir
+      if [ (ls -A $dir | wc -l) -gt 0 ]
+        set dir_list_filtered $dir_list_filtered $dir
+      end
+    end
+  end
+
+  # pass the list to _peco_change_directory function
+  for dir in $dir_list_filtered
+    echo $dir
+  end | _peco_change_directory $argv
 end
