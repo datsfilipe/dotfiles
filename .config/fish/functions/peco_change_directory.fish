93e7c248a1000858756543d5926a6bb5703432ac
datsfilipe <datsfilipe.foss@proton.me>
2023-05-18 11:22:03 -0300

    fix: change dir listing files

--- .config/fish/functions/peco_change_directory.fish
+++ .config/fish/functions/peco_change_directory.fish
@@ -19,7 +19,7 @@ function peco_change_directory
     echo $HOME/.config
     ghq list -p
     ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/www/s/*/*/* |grep -v \.git
+    ls -ad $HOME/www/s/*/* |grep -v \.git
     # ls -ad $HOME/www/w/*/*/* |grep -v \.git
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
