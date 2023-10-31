9fa0833bb4e8937f650c0a4e4d17c23189e420be
datsfilipe <datsfilipe.foss@proton.me>
2023-10-31 10:57:07 -0300

    feat: add colorschemes module

---
+++ modules/colorschemes.nix
@@ -0,0 +1,16 @@
+{
+  scheme = {
+    eva = {
+      bg = "#151515";
+      fg = "#c1c1c1";
+      black = "#585858";
+      red = "#FF7A84";
+      green = "#22863a";
+      yellow = "#ffab70";
+      blue = "#79b8ff";
+      magenta = "#b392f0";
+      cyan = "#9db1c5";
+      white = "#fafafa";
+    };
+  };
+}
