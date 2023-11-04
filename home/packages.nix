ebc9ae4bf1cd28b0dfd62cb2c2c99f4567b1918b
datsfilipe <datsfilipe.foss@proton.me>
2023-11-04 12:23:54 -0300

    feat: add fzf export variables in zsh config

--- home/packages.nix
+++ home/packages.nix
@@ -13,7 +13,6 @@
     bat
     eza
     zoxide
-    ripgrep
     fzf
     trash-cli
     fd
--- home/zsh.nix
+++ home/zsh.nix
@@ -82,6 +82,8 @@ in
         }
 
         export WAKATIME_HOME="$HOME/.config"
+        export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git --color=always"
+        export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
       '';
       shellAliases = aliases;
 
