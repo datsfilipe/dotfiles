a884b715da49c8d20b6487d2ed0214f691b0f105
datsfilipe <datsfilipe.foss@proton.me>
2023-11-16 16:16:12 -0300

    refactor(ags): apply css differently

--- modules/ags/config.js
+++ modules/ags/config.js
@@ -3,8 +3,10 @@ import Bar from './js/bar/Bar.js';
 import PowerMenu from './js/powermenu/PowerMenu.js'
 import Verification from './js/powermenu/Verification.js'
 
+App.resetCss();
+App.applyCss(App.configDir + '/style.css');
+
 export default {
-  style: App.configDir + '/style.css',
   windows: [
     Bar(),
     PowerMenu(),
