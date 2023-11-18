d3817e1b5e4e4dda34db006bffede3ec1232cfd5
datsfilipe <datsfilipe.foss@proton.me>
2023-11-18 17:16:49 -0300

    docs: remove comments in wezterm config

--- home/apps/wezterm.nix
+++ home/apps/wezterm.nix
@@ -6,8 +6,6 @@ in {
     local wezterm = require("wezterm")  
     local config = {}
 
-    -- In newer versions of wezterm, use the config_builder which will
-    -- help provide clearer error messages
     if wezterm.config_builder then
       config = wezterm.config_builder()
     end
