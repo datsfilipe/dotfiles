095ed7345926bf418d2ec953acae9ec55f332e47
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 23:43:27 -0300

    refactor: update nvim module compatibility with nix theme

--- home/apps/nvim.nix
+++ home/apps/nvim.nix
@@ -6,7 +6,11 @@ in {
     recursive = true;
   };
 
-  xdg.configFile."nvim/lua/nix-colorscheme.lua".text = ''
+  xdg.configFile."nvim/lua/utils/nix_colorscheme.lua".text = ''
     return "${theme.nvim-colorscheme}"
+  '';
+
+  xdg.configFile."nvim/lua/utils/nix_lazylock.lua".text = ''
+    return vim.fn.expand("$HOME/.dotfiles/modules/nvim") .. "/lazy-lock.json"
   '';
 }
