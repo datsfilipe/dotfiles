b9ac610669e4f53aa06609640b7f4344a5b55c0e
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 23:42:24 -0300

    feat: add solarized theme

---
+++ modules/colorscheme/solzarized.nix
@@ -0,0 +1,23 @@
+{
+  scheme = {
+    colors = {
+      bg = "#00181e";
+      altbg = "#002b36";
+      fg = "#839496";
+      black = "#073642";
+      red = "#dc322f";
+      green = "#859900";
+      yellow = "#b58900";
+      blue = "#268bd2";
+      magenta = "#d33682";
+      cyan = "#2aa198";
+      white = "#eee8d5";
+    };
+  };
+
+  nvim-colorscheme = "solarized-osaka";
+  gtk-theme = {
+    package = "flat-remix-gtk";
+    name = "Flat-Remix-GTK-Cyan-Darkest-Solid";
+  };
+}
