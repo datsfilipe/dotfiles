69247d31cf5703340cb38c0abbc7cbdafd27cf79
datsfilipe <datsfilipe.foss@proton.me>
2023-11-28 11:41:50 -0300

    fix: wrong path for scripts in run.sh file

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ USER=$(whoami)
 # setup unix-scripts for user
 rm -rf /home/$USER/.local/bin
 mkdir /home/$USER/.local/bin
-ln ./modules/scripts/* /home/$USER/.local/bin
+ln ./dotfiles/bin/* /home/$USER/.local/bin
 
 sudo nixos-rebuild switch --flake .?submodules=1#$USER-machine --impure
