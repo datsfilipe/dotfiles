d001eef1a8b9217216eb3ad9008e21aa1e3cb232
datsfilipe <datsfilipe.foss@proton.me>
2023-11-28 15:02:31 -0300

    chore: add buku database backup in run script

--- run.sh
+++ run.sh
@@ -7,4 +7,8 @@ rm -rf /home/$USER/.local/bin
 mkdir /home/$USER/.local/bin
 ln ./dotfiles/bin/* /home/$USER/.local/bin
 
+# backup buku database
+buku -l 8
+cp /home/$USER/.local/share/buku/bookmarks.db.enc ./dotfiles/buku/bookmarks.db.enc
+
 sudo nixos-rebuild switch --flake .?submodules=1#$USER-machine --impure
