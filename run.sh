bc67556da6ec66d09aedf3612cefe886c78f27b2
datsfilipe <datsfilipe.foss@proton.me>
2023-12-10 21:25:58 -0300

    style: update rebuild command

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ rm -rf /home/$USER/.local/bin
 mkdir /home/$USER/.local/bin
 ln ./dotfiles/bin/* /home/$USER/.local/bin
 
-sudo nixos-rebuild switch --flake .?submodules=1#$USER-machine --impure
+sudo nixos-rebuild switch --impure --flake .?submodules=1#$USER-machine
