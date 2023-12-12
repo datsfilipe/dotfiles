d62705f742fc160d6638bcc638a2afae9ae1a679
datsfilipe <datsfilipe.foss@proton.me>
2023-12-12 10:48:29 -0300

    refactor: update hyprland keybind to call alacritty

--- modules/desktops/hyprland/config.nix
+++ modules/desktops/hyprland/config.nix
@@ -75,7 +75,7 @@ let
 
     $mainMod=SUPER
 
-    bind=$mainMod,Return,exec,rio
+    bind=$mainMod,Return,exec,alacritty
     bind=$mainMod,A,exec,firefox
     bind=$mainMod,P,exec,ags --toggle-window launcher
     bind=$mainMod,O,exec,ags --toggle-window powermenu
