34d2f4454b68d7d691fffbc1386c531a14d41fb6
datsfilipe <datsfilipe.foss@proton.me>
2023-12-12 23:20:56 -0300

    feat: update flakes when running `run` script

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# update config submodules
+nix flake update datsnvim
+nix flake update unix-scripts
+nix flake update walls
+
 sudo nixos-rebuild switch --impure --flake .#dtsf-machine
