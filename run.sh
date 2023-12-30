d2e7c00eb05bbec7f54e8081666604527666314c
datsfilipe <datsfilipe.foss@proton.me>
2023-12-30 10:44:37 -0300

    feat: add ags dependencies installation in run.sh script

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+git_root=$(git rev-parse --show-toplevel)
+
+if [ "$PWD" != "$git_root" ]; then
+    echo "please run this script from the root of the git repo"
+    exit 1
+fi
+
 # update config submodules
 nix flake update datsnvim
 nix flake update unix-scripts
 nix flake update walls
 
 sudo nixos-rebuild switch --impure --flake .#dtsf-machine
+
+# update ags dependencies
+ags_root="$HOME/.config/ags"
+pushd $ags_root || exit 1
+
+if [ ! -d "$ags_root/node_modules" ]; then
+  direnv exec "$HOME/.config/ags" pnpm i
+fi
+
+popd || exit 1
