cfd062d751d560f9282e2e7de1bddd8734c4920a
datsfilipe <datsfilipe.foss@proton.me>
2023-12-31 16:50:17 -0300

    refactor: change dunst bg color for altbg

--- home/services/dunst.nix
+++ home/services/dunst.nix
@@ -4,16 +4,16 @@ let theme = (import ../../modules/colorscheme).theme;
 in {
   xdg.configFile."dunst/dunstrc".text = ''
     ${lib.fileContents ../../dotfiles/dunstrc}
-      frame_color = "${theme.scheme.colors.bg}"
-      separator_color = "${theme.scheme.colors.bg}"
+      frame_color = "${theme.scheme.colors.altbg}"
+      separator_color = "${theme.scheme.colors.altbg}"
 
     [urgency_low]
-      background = "${theme.scheme.colors.bg}"
+      background = "${theme.scheme.colors.altbg}"
       foreground = "${theme.scheme.colors.red}"
       timeout = 5
 
     [urgency_normal]
-      background = "${theme.scheme.colors.bg}"
+      background = "${theme.scheme.colors.altbg}"
       foreground = "${theme.scheme.colors.red}"
       timeout = 10
 
