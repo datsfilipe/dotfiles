e909a15c291f9ee4b5ca065e6d325b5d26e4fd2f
datsfilipe <datsfilipe.foss@proton.me>
2024-01-16 12:06:11 -0300

    feat: add kanagawa coloscheme

---
+++ modules/colorscheme/kanagawa.nix
@@ -0,0 +1,24 @@
+{
+  scheme = {
+    colors = {
+      primary = "#c4746e";
+      bg = "#181616";
+      altbg = "#2D4F67";
+      fg = "#c5c9c5";
+      black = "#585858";
+      red = "#c4746e";
+      green = "#8a9a7b";
+      yellow = "#c4b28a";
+      blue = "#8ba4b0";
+      magenta = "#a292a3";
+      cyan = "#8ea4a2";
+      white = "#C8C093";
+    };
+  };
+
+  nvim-colorscheme = "kanagawa";
+  gtk-theme = {
+    package = "flat-remix-gtk";
+    name = "Flat-Remix-GTK-Orange-Darkest-Solid";
+  };
+}
