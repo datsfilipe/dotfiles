8c5a2bc42e85801b9a67713ed5baca673f07da55
datsfilipe <datsfilipe.foss@proton.me>
2024-01-22 01:31:28 -0300

    refactor: update alacritty configs to toml

--- home/apps/alacritty.nix
+++ home/apps/alacritty.nix
@@ -2,34 +2,33 @@
 
 let theme = (import ../../modules/colorscheme).theme;
 in {
-  xdg.configFile."alacritty/alacritty.yml".text = ''
-    ${lib.fileContents ../../dotfiles/alacritty.yml}
+  xdg.configFile."alacritty/alacritty.toml".text = ''
+    ${lib.fileContents ../../dotfiles/alacritty.toml}
   '';
 
-  xdg.configFile."alacritty/colors.yml".text = ''
-    colors:
-      primary:
-        background: "${theme.scheme.colors.bg}"
-        foreground: "${theme.scheme.colors.fg}"
+  xdg.configFile."alacritty/colors.toml".text = ''
+    [colors.bright]
+    black = "${theme.scheme.colors.black}"
+    red = "${theme.scheme.colors.red}"
+    green = "${theme.scheme.colors.green}"
+    yellow = "${theme.scheme.colors.yellow}"
+    blue = "${theme.scheme.colors.blue}"
+    magenta = "${theme.scheme.colors.magenta}"
+    cyan = "${theme.scheme.colors.cyan}"
+    white = "${theme.scheme.colors.white}"
 
-      normal:
-        black: "${theme.scheme.colors.black}"
-        red: "${theme.scheme.colors.red}"
-        green: "${theme.scheme.colors.green}"
-        yellow: "${theme.scheme.colors.yellow}"
-        blue: "${theme.scheme.colors.blue}"
-        magenta: "${theme.scheme.colors.magenta}"
-        cyan: "${theme.scheme.colors.cyan}"
-        white: "${theme.scheme.colors.white}"
+    [colors.normal]
+    black = "${theme.scheme.colors.black}"
+    red = "${theme.scheme.colors.red}"
+    green = "${theme.scheme.colors.green}"
+    yellow = "${theme.scheme.colors.yellow}"
+    blue = "${theme.scheme.colors.blue}"
+    magenta = "${theme.scheme.colors.magenta}"
+    cyan = "${theme.scheme.colors.cyan}"
+    white = "${theme.scheme.colors.white}"
 
-      bright:
-        black: "${theme.scheme.colors.black}"
-        red: "${theme.scheme.colors.red}"
-        green: "${theme.scheme.colors.green}"
-        yellow: "${theme.scheme.colors.yellow}"
-        blue: "${theme.scheme.colors.blue}"
-        magenta: "${theme.scheme.colors.magenta}"
-        cyan: "${theme.scheme.colors.cyan}"
-        white: "${theme.scheme.colors.white}"
+    [colors.primary]
+    background = "${theme.scheme.colors.bg}"
+    foreground = "${theme.scheme.colors.fg}"
   '';
 }
