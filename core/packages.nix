1917a48d7abe4174383acd2454d2e8c5117150ab
datsfilipe <datsfilipe.foss@proton.me>
2024-01-22 01:55:28 -0300

    chore: install mpv from home manager

--- core/packages.nix
+++ core/packages.nix
@@ -56,7 +56,6 @@ in {
     
     # media
     pavucontrol
-    mpv
     ffmpeg
   ] ++ customPkgs;
 }
--- home/apps/mpv.nix
+++ home/apps/mpv.nix
@@ -1,6 +1,13 @@
+{ pkgs, ... }:
+
 {
   xdg.configFile."mpv/scripts" = {
     source = ../../dotfiles/mpv;
     recursive = true;
+  };
+
+  programs.mpv = {
+    enable = true;
+    package = pkgs.mpv;
   };
 }
