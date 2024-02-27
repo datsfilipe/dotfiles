16046f031d3d19705fd861ef43f0f73beaa9450d
datsfilipe <datsfilipe.foss@proton.me>
2024-02-27 09:42:16 -0300

    feat: add function for creating a tunnel

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -23,6 +23,12 @@ in {
         tmux new-session -d -s vault
         tmux switch-client -t vault
       '';
+      dtc = ''
+        devtunnel create $argv[1]
+        devtunnel port create $argv[1] -p $argv[2]
+        devtunnel access create $argv[1] -p $argv[2] --anonymous
+        devtunnel host $argv[1]
+      '';
     };
 
     shellInit = ''
