a0818a43131d998ce85a3fa130ecd26018081be1
datsfilipe <datsfilipe.foss@proton.me>
2024-03-02 00:29:41 -0300

    update run script

--- run.sh
+++ run.sh
@@ -22,10 +22,6 @@ if [ ! -d "$ags_root/node_modules" ]; then
   direnv exec "$HOME/.config/ags" pnpm i
 fi
 
-# remove spaceship prompt file
-# rm "$HOME/.config/spaceship.zsh.zwc"
-
-# rebuild bat cache
-# bat cache --build
+bat cache --build
 
 popd || exit 1
