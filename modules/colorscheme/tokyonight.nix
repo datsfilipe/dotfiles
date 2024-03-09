016a5601f7d3be158798d0d3bfa47b06adcb33d7
datsfilipe <datsfilipe.foss@proton.me>
2024-03-08 23:46:51 -0300

    feat: add `tokyonight` to nixos colorschemes

---
+++ modules/colorscheme/tokyonight.nix
@@ -0,0 +1,25 @@
+{
+  scheme = {
+    colors = {
+      primary = "#7aa2f7";
+      bg = "#15161e";
+      altbg = "#213464";
+      fg = "#e1e2e7";
+      black = "#414868";
+      red = "#db4b4b";
+      green = "#9ece6a";
+      yellow = "#e0af68";
+      blue = "#7aa2f7";
+      magenta = "#bb9af7";
+      cyan = "#7dcfff";
+      white = "#c0caf5";
+    };
+  };
+
+  nvim-colorscheme = "tokyonight";
+  gtk-theme = {
+    package = "flat-remix-gtk";
+    name = "Flat-Remix-GTK-Blue-Darkest-Solid";
+  };
+  tmux-border-color = "#7aa2f7";
+}
