e34c065930b8fbb4fdb7df94f18134f895082d72
datsfilipe <datsfilipe.foss@proton.me>
2024-04-06 06:52:06 -0300

    fix: popup window not showing nothing

--- dotfiles/ags/js/misc/PopupWindow.js
+++ dotfiles/ags/js/misc/PopupWindow.js
@@ -5,54 +5,40 @@ const Padding = (windowName) =>
     className: 'padding',
     hexpand: true,
     vexpand: true,
-    connections: [['button-press-event', () => App.toggleWindow(windowName)]],
+    setup: () => App.toggleWindow(windowName),
   })
 
 const PopupRevealer = (windowName, transition, child) =>
   Widget.Box({
-    style: 'padding: 1px;',
+    css: 'padding: 1px;',
     child: Widget.Revealer({
       transition,
       child,
       transitionDuration: 250,
-      connections: [
-        [
-          App,
-          (revealer, name, visible) => {
-            if (name === windowName) revealer.reveal_child = visible
-          },
-        ],
-      ],
-    }),
+      setup: self => self.hook(App, (revealer, name, visible) => {
+        if (windowName !== name) return
+        revealer.reveal_child = visible
+      }),
+    })
   })
 // "min-width: 5000px; min-height: 3000px; background-color: black;"
 const layouts = {
-  center: (windowName, child, expand) =>
+  center: (_, child, expand) =>
     Widget.CenterBox({
       className: 'shader' + expand ? ' popup-bg black' : '',
-      // style: expand ? "min-width: 5000px; min-height: 3000px; background-color: black;" : "",
-      children: [
-        Padding(windowName),
-        Widget.CenterBox({
-          vertical: true,
-          children: [Padding(windowName), child, Padding(windowName)],
-        }),
-        Padding(windowName),
-      ],
+      centerWidget: Widget.CenterBox({
+        vertical: true,
+        centerWidget: child,
+      }),
     }),
   top: (windowName, child) =>
     Widget.CenterBox({
-      children: [
-        Padding(windowName),
-        Widget.Box({
-          vertical: true,
-          children: [
-            PopupRevealer(windowName, 'slide_down', child),
-            Padding(windowName),
-          ],
-        }),
-        Padding(windowName),
-      ],
+      centerWidget: Widget.Box({
+        vertical: true,
+        children: [
+          PopupRevealer(windowName, 'slide_down', child),
+        ],
+      }),
     }),
   'top right': (windowName, child) =>
     Widget.Box({
@@ -74,8 +60,7 @@ export default ({ layout = 'center', expand = true, name, content, ...rest }) =>
   Widget.Window({
     name,
     child: layouts[layout](name, content, expand),
-    popup: true,
     visible: false,
-    focusable: true,
+    keymode: 'on-demand',
     ...rest,
-  })
+  }).keybind('Escape', () => App.closeWindow(name))
