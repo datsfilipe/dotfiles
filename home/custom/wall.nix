8a58fc95b4cb5ff323aeb4ee3355209f3aaa17f9
datsfilipe <datsfilipe.foss@proton.me>
2024-04-11 01:37:22 -0300

    refactor: update theme and wall

--- home/custom/wall.nix
+++ home/custom/wall.nix
@@ -1,5 +1,5 @@
 { inputs, ... }:
 
 {
-  xdg.configFile."wallpaper.png".source = "${inputs.walls}/14.png";
+  xdg.configFile."wallpaper.png".source = "${inputs.walls}/15.png";
 }
--- modules/colorscheme/default.nix
+++ modules/colorscheme/default.nix
@@ -1,3 +1,3 @@
 {
-  theme = import ./eva.nix;
+  theme = import ./vesper.nix;
 }
