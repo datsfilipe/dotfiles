c5f59ace043d13391adafe13d250dceb0240becb
datsfilipe <datsfilipe.foss@proton.me>
2024-06-15 01:11:07 -0300

    feat: add `pre-push` hook for linting

--- shells/work/flake.nix
+++ shells/work/flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -18,7 +19,24 @@
           libuuid
         ];
       in with pkgs; {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              lint = {
+                enable = true;
+                name = "lint";
+                entry = "yarn lint --fix";
+                files = "\\.(js|ts|jsx|tsx)$";
+                stages = ["pre-push"];
+              };
+            };
+          };
+        };
+
         devShells.default = mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
           name = "nodejs";
           packages = inputs;
           env = {
