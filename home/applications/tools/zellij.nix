6f27a960d9502b677465288f6a75c647a3451e06
datsfilipe <datsfilipe.foss@proton.me>
2024-07-14 03:16:10 -0300

    feat: configure `zellij` keybindings

--- home/applications/tools/zellij.nix
+++ home/applications/tools/zellij.nix
@@ -1,30 +1,30 @@
-{ theme, ... }:
+{ lib, theme, vars, ... }:
 
-{
+with lib; {
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
-    settings = {
-      # general
-      default_layout = "compact";
-      pane_frames = false;
-      simplified_ui = true;
-      copy_on_select = false;
-      session_serialization = false;
-
-      # theme
-      theme = "custom";
-      themes.custom.fg = theme.scheme.colors.fg;
-      themes.custom.bg = theme.scheme.colors.bg;
-      themes.custom.black = theme.scheme.colors.bg;
-      themes.custom.red = theme.scheme.colors.red;
-      themes.custom.green = theme.scheme.colors.black;
-      themes.custom.yellow = theme.scheme.colors.yellow;
-      themes.custom.blue = theme.scheme.colors.blue;
-      themes.custom.magenta = theme.scheme.colors.magenta;
-      themes.custom.cyan = theme.scheme.colors.cyan;
-      themes.custom.white = theme.scheme.colors.white;
-      themes.custom.orange = theme.scheme.colors.green;
-    };
   };
+
+  xdg.configFile."zellij/config.kdl".text = ''
+    default_shell "${vars.environment.shell}"
+    theme "custom"
+    themes {
+      custom {
+        bg "${theme.scheme.colors.bg}"
+        black "${theme.scheme.colors.bg}"
+        blue "${theme.scheme.colors.blue}"
+        cyan "${theme.scheme.colors.cyan}"
+        fg "${theme.scheme.colors.fg}"
+        green "${theme.scheme.colors.black}"
+        magenta "${theme.scheme.colors.magenta}"
+        orange "${theme.scheme.colors.green}"
+        red "${theme.scheme.colors.red}"
+        white "${theme.scheme.colors.white}"
+        yellow "${theme.scheme.colors.yellow}"
+      }
+    }
+
+    ${fileContents ../../../dotfiles/zellij/config.kdl}
+  '';
 }
