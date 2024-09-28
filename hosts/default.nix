8ff6d445840f017376e7891772838faa1e2bdcd1
datsfilipe <datsfilipe.foss@proton.me>
2024-09-28 02:12:33 -0300

    fix: add `dtsf-book` option in hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,5 +3,6 @@
 with mylib; {
   host = utils.match { host = vars.host or "dtsf-machine"; } [
     [{ host = "dtsf-machine"; } (import ./dtsf-machine)]
+    [{ host = "dtsf-book"; } (import ./dtsf-book)]
   ];
 }
