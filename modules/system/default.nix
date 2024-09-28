1f685d93b95d8e44b75b58b6b35a036de4e5f357
datsfilipe <datsfilipe.foss@proton.me>
2024-09-28 00:30:49 -0300

    refactor: update `modules/system` to support multiple hosts

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -16,38 +16,36 @@ let
   lib = nixpkgs.lib;
   mylib = (import ../../lib { inherit lib builtins; });
   theme = (import ../colorscheme { inherit mylib vars; }).theme;
-in
-{
-  dtsf-machine = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs vars mylib theme; };
+in lib.nixosSystem {
+  inherit system;
+  specialArgs = { inherit inputs vars mylib theme; };
 
-    modules = [
-      ./configuration.nix
-      ./packages.nix
-      home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          extraSpecialArgs = { inherit inputs vars mylib theme; };
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.${vars.user} = {
-            imports = (
-              import ../../home
-            );
+  modules = [
+    ./configuration.nix
+    ./packages.nix
+    home-manager.nixosModules.home-manager
+    {
+      home-manager = {
+        extraSpecialArgs = { inherit inputs vars mylib theme; };
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${vars.user} = {
+          imports = (
+            import ../../home
+          );
 
-            home.sessionPath = [
-              "$HOME/.local/bin"
-            ];
+          home.sessionPath = [
+            "$HOME/.local/bin"
+          ];
 
-            home.stateVersion = "24.05";
-            programs.home-manager.enable = true;
-          };
+          home.stateVersion = "24.05";
+          programs.home-manager.enable = true;
         };
-      }
+      };
+    }
 
-      # grub theme
-      inputs.minegrub-theme.nixosModules.default
-    ];
-  };
+    # grub theme
+    inputs.minegrub-theme.nixosModules.default
+  ];
 }
+
