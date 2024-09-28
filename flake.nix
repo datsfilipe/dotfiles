3215732b80ac14112bd6135140cff5b2576f2cfb
datsfilipe <datsfilipe.foss@proton.me>
2024-09-28 00:32:17 -0300

    feat: support multiple hosts in `flake.nix` and import correct config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dats NixOS dotfiles from datsfilipe.";
+  description = "dats nixos dotfiles from datsfilipe.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -21,25 +21,17 @@
     # third party
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
-    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
-    ags.url = "github:Aylur/ags";
+    minegrub-theme.url = "github:lxtharia/minegrub-theme";
+    ags.url = "github:aylur/ags";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
       lib = nixpkgs.lib;
-      helpers = import ./helpers.nix { inherit lib; };
-
-      mkHost = hostname:
+      mkhost = hostname:
         let
           vars = import (./hosts + "/${hostname}/host-configuration.nix") { inherit lib; };
-        in
-        lib.nixosSystem (
+        in (
           import ./modules/system {
             inherit nixpkgs lib inputs home-manager vars;
           }
@@ -47,8 +39,8 @@
     in
     {
       nixosConfigurations = {
-        "dtsf-machine" = mkHost "dtsf-machine";
-        "dtsf-book" = mkHost "dtsf-book";
+        "dtsf-machine" = mkhost "dtsf-machine";
+        "dtsf-book" = mkhost "dtsf-book";
       };
     };
 }
