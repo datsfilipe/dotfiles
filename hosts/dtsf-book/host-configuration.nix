ab46dad6179644b2bbf1a9324475ef96b01b34f5
datsfilipe <datsfilipe.foss@proton.me>
2024-09-29 19:02:18 -0300

    refactor: cleaner host config for dtsf-book

--- hosts/dtsf-book/host-configuration.nix
+++ hosts/dtsf-book/host-configuration.nix
@@ -7,10 +7,16 @@ in
 lib.recursiveUpdate common {
   user = user;
   host = "dtsf-book";
-  system.load_nvidia_module = false;
-  system.ollama = false;
-  system.boot = "systemd";
-  system.dpi = "144";
-  appearance.bg.wall = "25.png";
-  appearance.bg.folder = "/home/${user}/media/photos/wallpapers";
+  system = {
+    load_nvidia_module = false;
+    ollama = false;
+    boot = "systemd";
+    dpi = "144";
+  };
+  appearance = {
+    bg = {
+      wall = "25.png";
+      folder = "/home/${user}/media/photos/wallpapers";
+    };
+  };
 }
