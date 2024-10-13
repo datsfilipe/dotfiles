52ca1461d02682622a92cdefb25306c85ee7b191
datsfilipe <datsfilipe.foss@proton.me>
2024-10-12 21:12:01 -0300

    feat: add fractional scaling on gnome

--- modules/desktops/gnome/default.nix
+++ modules/desktops/gnome/default.nix
@@ -3,7 +3,13 @@
 lib.mkIf (vars.environment.desktop == "gnome") {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome = {
+    enable = true;
+    extraGSettingsOverrides = ''
+      [org.gnome.mutter]
+      experimental-features=['scale-monitor-framebuffer']
+    '';
+  };
 
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
