c0160de51a531b6e1a75f8ce6ea482707e4bac43
datsfilipe <datsfilipe.foss@proton.me>
2025-01-03 12:14:19 -0300

    feat: add home config for zellij

--- home/base/tui/zellij/default.nix
+++ home/base/tui/zellij/default.nix
@@ -8,5 +8,12 @@ in {
     enableFishIntegration = true;
   };
 
+  xdg.configFile."zellij/config.kdl".source = ./conf/config.kdl;
+  xdg.configFile."zellij/layouts/default.kdl".source = ./conf/layout.kdl;
+
+  home.sessionVariables = {
+    ZELLIJ_SWITCH_PATH = builtins.toString ./conf/zellij-switch.wasm;
+  };
+
   home.shellAliases = shellAliases;
 }
