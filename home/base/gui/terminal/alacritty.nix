7fb296d5340548e72d32ade7d61f630d4ca09ac3
datsfilipe <datsfilipe.foss@proton.me>
2025-01-03 12:47:59 -0300

    feat: add alacritty config

--- home/base/gui/terminal/alacritty.nix
+++ home/base/gui/terminal/alacritty.nix
@@ -3,6 +3,32 @@
 with lib; {
   programs.alacritty = {
     enable = true;
+    settings = {
+      terminal.shell.program = "fish";
+
+      font = {
+        size = 14;
+        bold = { family = "JetBrainsMono Nerd Font"; style = "Bold"; };
+        normal = { family = "JetBrainsMono Nerd Font"; style = "Regular"; };
+        italic = { family = "JetBrainsMono Nerd Font"; style = "Italic"; };
+        bold_italic = { family = "JetBrainsMono Nerd Font"; style = "Bold Italic"; };
+      };
+
+      cursor = {
+        style = { shape = "Block"; blinking = "On"; };
+        blink_interval = 500;
+      };
+
+      keyboard.bindings = [
+        { key = "X"; mods = "Control | Shift"; action = "ToggleViMode"; }
+      ];
+
+      window = {
+        opacity = 0.8;
+        padding.x = 25;
+        padding.y = 25;
+      };
+    };
   };
 
   # xdg.configFile."alacritty/alacritty.toml".text = ''
