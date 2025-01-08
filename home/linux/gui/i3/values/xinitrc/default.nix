637e5fbc7148cd080728197d341097d5bab72427
Filipe Lima <datsfilipe.foss@proton.me>
2025-01-08 01:53:29 -0300

    feat: add cursor config in xinitrc

--- home/linux/gui/i3/values/xinitrc/default.nix
+++ home/linux/gui/i3/values/xinitrc/default.nix
@@ -2,6 +2,7 @@
   home.file.".xinitrc".text = ''
     ${fileContents ./conf/mergex-conf.sh}
 
+    exec ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr &
     exec ${pkgs.picom}/bin/picom -f &
     exec ${pkgs.i3}/bin/i3
   '';
