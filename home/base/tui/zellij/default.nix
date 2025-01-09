9b0e3289364ed15f0fb1a236a0ed6750ee40834a
Filipe Lima <datsfilipe.foss@proton.me>
2025-01-09 10:30:12 -0300

    feat: replace zellij commands to work properly

--- home/base/tui/zellij/default.nix
+++ home/base/tui/zellij/default.nix
@@ -1,11 +1,21 @@
-{mypkgs, ...}: let
+{mypkgs, config, lib, ...}: let
   shellAliases = {
     "zj" = "zellij";
   };
+
+  scriptpath = script: "${config.home.homeDirectory}/.local/bin/${script}";
+  pluginpath = "${mypkgs.zellij-switch}/bin/zellij-switch.wasm";
+
+  replacer = script: text: lib.replaceStrings ["Run \"${script}\""] [
+    "Run \"${scriptpath script}\" \"--plugin\" \"${pluginpath}\""
+  ] text;
 in {
   programs.zellij.enable = true;
   modules.desktop.conf.zellij = {
-    content = builtins.readFile ./conf/config.kdl;
+    content =
+      (replacer "zellij-session-nav"
+        (replacer "zellij-sessionizer"
+          (builtins.readFile ./conf/config.kdl)));
     layoutContent = builtins.readFile ./conf/layout.kdl;
   };
 
