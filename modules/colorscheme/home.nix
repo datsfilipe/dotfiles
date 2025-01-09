a1acd8a0d242486ae8bd4e2fbbb6903ce35d41e9
Filipe Lima <datsfilipe.foss@proton.me>
2025-01-09 05:22:24 -0300

    feat: improve readability on colorscheme module

--- modules/colorscheme/home.nix
+++ modules/colorscheme/home.nix
@@ -8,21 +8,9 @@
       description = "Colorscheme theme.";
     };
 
-    enableAlacrittyIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
-    enableDunstIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
-    enableCavaIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
+    enableAlacrittyIntegration = lib.mkOption { type = lib.types.bool; default = false; };
+    enableDunstIntegration = lib.mkOption { type = lib.types.bool; default = false; };
+    enableCavaIntegration = lib.mkOption { type = lib.types.bool; default = false; };
     enableNeovimIntegration = lib.mkOption {
       type = lib.types.bool;
       default = true;
@@ -31,27 +19,10 @@
         file to the nvim lua directory with the colorscheme name only.
       '';
     };
-
-    enableZellijIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
-    enableGTKIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
-    enableI3Integration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
-    enableI3StatusIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-
+    enableZellijIntegration = lib.mkOption { type = lib.types.bool; default = false; };
+    enableGTKIntegration = lib.mkOption { type = lib.types.bool; default = false; };
+    enableI3Integration = lib.mkOption { type = lib.types.bool; default = false; };
+    enableI3StatusIntegration = lib.mkOption { type = lib.types.bool; default = false; };
     enableI3LockIntegration = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -60,67 +31,78 @@
         to the user ".local/bin" folder, which should be used instead of i3lock.
       '';
     };
+    enableFishIntegration = lib.mkOption { type = lib.types.bool; default = false; };
+  };
 
-    enableFishIntegration = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    }; };
+  config =
+    let
+      colorscheme = import ./themes/${config.modules.desktop.colorscheme.theme}.nix;
+    in
+    lib.mkMerge [
+      (lib.mkIf (config.modules.desktop.colorscheme.enable) (lib.mkMerge [
+        (lib.mkIf config.modules.desktop.colorscheme.enableNeovimIntegration {
+          xdg.configFile."nvim/lua/nix_colorscheme.lua".text = ''
+            return "${(import ./integrations/neovim.nix {
+              inherit mylib;
+              name = config.modules.desktop.colorscheme.theme;
+            })}";
+          '';
+        })
 
-  config = lib.mkMerge [
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableNeovimIntegration) {
-      xdg.configFile."nvim/lua/nix_colorscheme.lua".text = ''
-        return "${(import ./integrations/neovim.nix { inherit mylib; name = config.modules.desktop.colorscheme.theme; })}"
-      '';
-    })
+        (lib.mkIf config.modules.desktop.colorscheme.enableGTKIntegration {
+          gtk = import ./integrations/gtk.nix {
+            inherit mylib pkgs;
+            name = config.modules.desktop.colorscheme.theme;
+          };
+        })
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableGTKIntegration) {
-      gtk = (import ./integrations/gtk.nix { inherit mylib pkgs; name = config.modules.desktop.colorscheme.theme; });
-    })
+        (lib.mkIf config.modules.desktop.colorscheme.enableI3LockIntegration
+          (import ./integrations/i3lock.nix {
+            inherit pkgs;
+            colorscheme = colorscheme;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableI3LockIntegration)
-      (import ./integrations/i3lock.nix { inherit pkgs; colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
+        (lib.mkIf config.modules.desktop.colorscheme.enableI3Integration
+          (import ./integrations/i3.nix {
+            inherit lib pkgs;
+            colorscheme = colorscheme;
+            enableI3StatusIntegration = config.modules.desktop.colorscheme.enableI3StatusIntegration;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableI3Integration)
-      (import ./integrations/i3.nix {
-        inherit lib pkgs;
-        colorscheme = (
-          import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-        );
-        enableI3StatusIntegration = config.modules.desktop.colorscheme.enableI3StatusIntegration;
-      })
-    )
+        (lib.mkIf config.modules.desktop.colorscheme.enableDunstIntegration
+          (import ./integrations/dunst.nix {
+            colorscheme = colorscheme;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableDunstIntegration)
-      (import ./integrations/dunst.nix { colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
+        (lib.mkIf config.modules.desktop.colorscheme.enableAlacrittyIntegration
+          (import ./integrations/alacritty.nix {
+            colorscheme = colorscheme;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableAlacrittyIntegration)
-      (import ./integrations/alacritty.nix { colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
+        (lib.mkIf config.modules.desktop.colorscheme.enableFishIntegration
+          (import ./integrations/fish.nix {
+            inherit config lib;
+            colorscheme = colorscheme;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableFishIntegration)
-      (import ./integrations/fish.nix { inherit config lib; colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
+        (lib.mkIf config.modules.desktop.colorscheme.enableCavaIntegration
+          (import ./integrations/cava.nix {
+            inherit config lib mylib;
+            colorscheme = colorscheme;
+          })
+        )
 
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableCavaIntegration)
-      (import ./integrations/cava.nix { inherit config lib mylib; colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
-
-    (lib.mkIf (config.modules.desktop.colorscheme.enable && config.modules.desktop.colorscheme.enableZellijIntegration)
-      (import ./integrations/zellij.nix { inherit config lib; colorscheme = (
-        import ./themes/${config.modules.desktop.colorscheme.theme}.nix
-      ); })
-    )
-  ];
+        (lib.mkIf config.modules.desktop.colorscheme.enableZellijIntegration
+          (import ./integrations/zellij.nix {
+            inherit config lib;
+            colorscheme = colorscheme;
+          })
+        )
+      ]))
+    ];
 }
