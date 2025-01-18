78e20ea82c0fecfa0f1eb389d3529a5023eb6893
Filipe Lima <datsfilipe.foss@proton.me>
2025-01-18 16:47:16 -0300

    chore: add hardware acceleration option although chrome is still buggy

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -46,7 +46,7 @@ in {
         pathsToLink = ["/libexec"];
         loginShellInit = ''
           if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-            exec sway --unsupported-gpu
+            exec sway --unsupported-gpu -Dlegacy-wl-drm
           fi
         '';
       };
