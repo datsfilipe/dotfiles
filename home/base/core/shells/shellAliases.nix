c840a9501b4666e4d2ef5bb70a931f893cf3d9a6
Filipe Lima <datsfilipe.foss@proton.me>
2025-02-16 13:08:14 -0300

    chore: format shellAliases.nix

--- home/base/core/shells/shellAliases.nix
+++ home/base/core/shells/shellAliases.nix
@@ -1,33 +1,33 @@
 {
-  "ll"="eza --color always --icons -1glb -s name --git --sort date --group-directories-first";
-  "lla"="eza --color always --icons -1galb -s name --git --sort date --group-directories-first";
-  "tree"="eza --tree -D -L 3";
+  "ll" = "eza --color always --icons -1glb -s name --git --sort date --group-directories-first";
+  "lla" = "eza --color always --icons -1galb -s name --git --sort date --group-directories-first";
+  "tree" = "eza --tree -D -L 3";
 
-  "del"="trash";
-  "dels"="trash --list";
-  "delu"="trash --fzf";
-  "delr"="trash --restore";
-  "delc"="trash --cleanup";
+  "del" = "trash";
+  "dels" = "trash --list";
+  "delu" = "trash --fzf";
+  "delr" = "trash --restore";
+  "delc" = "trash --cleanup";
 
-  "top"="btop -p 1";
-  "topp"="btop";
+  "top" = "btop -p 1";
+  "topp" = "btop";
 
   "rm" = "echo \"use trash instead of rm\"";
   "cat" = "bat";
-  "zp"="zipper";
-  "ext"="extract";
-  "zj"="zellij";
-  "nsh"="nix-set-shell";
-  "dt"="devtunnel";
-  "dd"="cd ~/.dotfiles";
-  "wconn"="nmcli-wifi-connect";
+  "zp" = "zipper";
+  "ext" = "extract";
+  "zj" = "zellij";
+  "nsh" = "nix-set-shell";
+  "dt" = "devtunnel";
+  "dd" = "cd ~/.dotfiles";
+  "wconn" = "nmcli-wifi-connect";
 
-  "dc-list"="docker-helpers names";
-  "dc-ips"="docker-helpers ips";
-  "dc-exec"="docker-helpers ex";
-  "dc-log"="docker-helpers logs";
-  "dc-rm"="docker-helpers srm";
-  "dc-rma"="docker-helpers rall";
-  "dc-img-rm"="docker-helpers riall";
-  "dc-stop"="docker-helpers sall";
+  "dc-list" = "docker-helpers names";
+  "dc-ips" = "docker-helpers ips";
+  "dc-exec" = "docker-helpers ex";
+  "dc-log" = "docker-helpers logs";
+  "dc-rm" = "docker-helpers srm";
+  "dc-rma" = "docker-helpers rall";
+  "dc-img-rm" = "docker-helpers riall";
+  "dc-stop" = "docker-helpers sall";
 }
