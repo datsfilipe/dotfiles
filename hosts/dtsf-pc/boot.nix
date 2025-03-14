084f26933f96c5e6a665e047d92c604073d9a2c5
Filipe Lima <datsfilipe.foss@proton.me>
2025-03-14 19:00:59 -0300

    feat: configure windows dual boot with grub

--- hosts/dtsf-pc/boot.nix
+++ hosts/dtsf-pc/boot.nix
@@ -1,4 +1,29 @@
 {pkgs, ...}: {
-  boot.loader.systemd-boot.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.loader = {
+    systemd-boot.enable = false;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true;
+      extraEntries = ''
+        menuentry "Windows" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          insmod chain
+          search --fs-uuid --set=root F8D5-8832
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+      version = 2;
+    };
+  };
+
+  time.hardwareClockInLocalTime = true;
 }
