4ff2b74e3951c0d70896a13f835144dacce4202f
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-12 03:06:02 -0300

    fix: remove comments and configure ripgrep args

--- home/base/core/core.nix
+++ home/base/core/core.nix
@@ -2,15 +2,19 @@
   home.packages = with pkgs; [
     gnupg
     gnumake
-    fzf
     fd
-    (ripgrep.override {withPCRE2 = true;})
     just
     hyperfine
     nix-index
   ];
 
   programs = {
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+
     eza = {
       enable = true;
       git = true;
@@ -21,28 +25,15 @@
       enable = true;
       config = {
         pager = "less -FR";
-        # theme = "catppuccin-mocha";
       };
-      # themes = {
-      # };
     };
 
-    fzf = {
+    ripgrep = {
       enable = true;
-      # colors = {
-      #   "bg+" = "#313244";
-      #   "bg" = "#1e1e2e";
-      #   "spinner" = "#f5e0dc";
-      #   "hl" = "#f38ba8";
-      #   "fg" = "#cdd6f4";
-      #   "header" = "#f38ba8";
-      #   "info" = "#cba6f7";
-      #   "pointer" = "#f5e0dc";
-      #   "marker" = "#f5e0dc";
-      #   "fg+" = "#cdd6f4";
-      #   "prompt" = "#cba6f7";
-      #   "hl+" = "#f38ba8";
-      # };
+      package = pkgs.ripgrep.override {withPCRE2 = true;};
+      arguments = [
+        "--smart-case"
+      ];
     };
   };
 }
