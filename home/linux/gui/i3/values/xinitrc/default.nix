60f9330c93695b5139d425a90766c0ad8c71bf55
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-13 00:33:48 -0300

    feat: import user variables with systemctl on xinitrc

--- home/linux/gui/i3/values/xinitrc/default.nix
+++ home/linux/gui/i3/values/xinitrc/default.nix
@@ -9,6 +9,7 @@ with lib; {
 
     ${fileContents ./conf/services.sh}
 
+    systemctl --user import-environment DISPLAY XAUTHORITY DBUS_SESSION_BUS_ADDRESS XDG_CURRENT_DESKTOP XDG_SESSION_TYPE WAYLAND_DISPLAY &
     exec ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr &
       exec ${pkgs.i3}/bin/i3
   '';
