b4317e4e0d146f265ecdad2efa4ad50028a46d3c
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-14 21:44:00 -0300

    chore: update trxsh package to get latest version always on update

--- modules/nupkgs/trxsh/conf/update.sh
+++ modules/nupkgs/trxsh/conf/update.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-URL="https://github.com/datsfilipe/trxsh/releases/download/v1.0.0/trxsh-v1.0.0-linux-amd64.tar.gz"
-
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/datsfilipe/trxsh/releases/latest)
+VERSION=$(echo "$LATEST_RELEASE" | jq -r .tag_name)
+URL="https://github.com/datsfilipe/trxsh/releases/download/${VERSION}/trxsh-${VERSION}-linux-amd64.tar.gz"
 HASH=$(nix-prefetch-url "$URL")
 NEW_HASH=$(nix hash convert --hash-algo sha256 --to base64 "$HASH")
-
 cat > "$SCRIPT_DIR/source.json" <<EOF
 {
   "sha256": "${NEW_HASH}",
-  "version": "v1.0.0"
+  "version": "${VERSION}"
 }
 EOF
-
-echo "Updated source.json with new hash: ${NEW_HASH}"
+echo "Updated source.json with new hash: ${NEW_HASH} for version: ${VERSION}"
