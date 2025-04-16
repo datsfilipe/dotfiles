4fabc04ed2fb97e6efbf314b72f6cb221816b061
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-16 19:51:58 -0300

    feat: configure double monitors with nvidia settings

--- modules/home/linux/gui/i3/values/xinitrc/default.nix
+++ modules/home/linux/gui/i3/values/xinitrc/default.nix
@@ -10,6 +10,10 @@ with lib; {
     ${fileContents ./conf/services.sh}
 
     systemctl --user import-environment DISPLAY XAUTHORITY DBUS_SESSION_BUS_ADDRESS XDG_CURRENT_DESKTOP XDG_SESSION_TYPE WAYLAND_DISPLAY &
+
+    nvidia-settings --assign \
+      CurrentMetaMode="DP-0: nvidia-auto-select +0+420, HDMI-0: 1920x1080+1920+0 {rotation=left}" &
+
     exec ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr &
       exec ${pkgs.i3}/bin/i3
   '';
