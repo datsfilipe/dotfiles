b5ed43d81c579396503c6f99ee24d8746a3ff494
Filipe Lima <datsfilipe.foss@proton.me>
2025-05-21 18:12:41 -0300

    refactor: try out something

--- hosts/dtsf-laptop/default.nix
+++ hosts/dtsf-laptop/default.nix
@@ -6,10 +6,6 @@ in {
     ./boot.nix
   ];
 
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
   networking = {
     inherit hostName;
     networkmanager.enable = true;
