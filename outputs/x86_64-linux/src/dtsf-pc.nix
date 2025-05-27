c785b1b21e1e204d447049f07b3f632abe08597f
Filipe Lima <datsfilipe.foss@proton.me>
2025-05-26 22:01:53 -0300

    refactor: update theme

--- outputs/x86_64-linux/src/dtsf-pc.nix
+++ outputs/x86_64-linux/src/dtsf-pc.nix
@@ -74,7 +74,7 @@
           modules.shared.multi-monitors = monitorsConfig;
           modules.desktop.wallpaper = {
             enable = true;
-            file = "/run/media/dtsf/datsgames/walls/37.png";
+            file = "/run/media/dtsf/datsgames/walls/41.png";
           };
           modules.ssh-key-manager.enable = true;
           modules.desktop.ollama.enable = true;
