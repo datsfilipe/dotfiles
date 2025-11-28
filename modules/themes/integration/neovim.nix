aeb151d8c63eadf123a531c2251c84ce0bc45562
Filipe Lima <datsfilipe.foss@proton.me>
2025-11-28 03:36:25 -0300

    fix: neovim theme integration

--- modules/themes/integration/neovim.nix
+++ modules/themes/integration/neovim.nix
@@ -1,14 +1,12 @@
-{
-  mylib,
-  name,
-  ...
-}:
-with mylib;
-  match {colorscheme = name;} [
-    [{colorscheme = "gruvbox";} "gruvbox"]
-    [{colorscheme = "min";} "min-theme"]
-    [{colorscheme = "solarized";} "solarized-osaka"]
-    [{colorscheme = "vesper";} "vesper"]
-    [{colorscheme = "catppuccin";} "catppuccin"]
-    [{colorscheme = "kanagawa";} "kanagawa"]
-  ]
+{name, ...}: let
+  colorschemeMap = {
+    catppuccin = "catppuccin";
+    kanagawa = "kanagawa";
+    min = "min-theme";
+    solarized = "solarized-osaka";
+    vesper = "vesper";
+    gruvbox = "gruvbox";
+  };
+  colorscheme = colorschemeMap.${name};
+in
+  colorscheme
