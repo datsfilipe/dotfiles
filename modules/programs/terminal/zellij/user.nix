648343f4b6ae007472262789d27e89caccfcc868
Filipe Lima <datsfilipe.foss@proton.me>
2025-12-04 11:57:38 -0300

    fix: missing option on scrollback editor

--- modules/programs/terminal/zellij/user.nix
+++ modules/programs/terminal/zellij/user.nix
@@ -23,7 +23,8 @@
     #!/bin/bash
     nvim --clean \
       -c "set relativenumber" \
-      -c "lua vim.g.clipboard = { name = 'smart-clipboard', copy = { ['+'] = 'shared-clipboard copy' }, paste = { ['+'] = 'shared-clipboard paste' } }" \
+      -c "lua vim.g.clipboard = { name = 'smart-clipboard', copy = { ['+'] = 'shared-clipboard copy' }, paste = { ['+'] = 'shared-clipboard paste' }, cache_enabled = 0 }" \
+      -c "set clipboard+=unnamedplus" \
       -c "highlight Normal guibg=NONE ctermbg=NONE" \
       + "$@"
   '';
