2783c2472d59a2af28685a7028253d794a86ad86
Filipe Lima <datsfilipe.foss@proton.me>
2025-12-25 21:42:02 -0300

    refactor: update hw config and deps; new installation

--- hosts/dtsf-pc/hardware-configuration.nix
+++ hosts/dtsf-pc/hardware-configuration.nix
@@ -9,27 +9,25 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "thunderbolt" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "thunderbolt" "usb_storage" "usbhid" "uas" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/3098c813-f5cd-40e5-a6f1-e3b4cbfd4c22";
+    device = "/dev/disk/by-uuid/3d76575b-5b0a-4185-a958-4b2bfc915178";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/33F4-8FFB";
+    device = "/dev/disk/by-uuid/23C1-5998";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/6e1abfe9-2fab-469f-8b7b-ce13f794c637";}
+    {device = "/dev/disk/by-uuid/a17e3ca6-7486-445c-839d-6b7011a0a8dd";}
   ];
-
-  networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
