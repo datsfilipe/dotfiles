6df2fd0224e8ce5e48f80208b9c5082dd6310ad6
Filipe Lima <datsfilipe.foss@proton.me>
2026-07-13 02:07:28 -0300

    fix: editor invocation not working properly

--- modules/editors/neovim/os.nix
+++ modules/editors/neovim/os.nix
@@ -1,16 +1,26 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib; let
   cfg = config.modules.editors.neovim.system;
+
+  editorScript = pkgs.writeShellScriptBin "nvim-editor" ''
+    exec nvim --clean \
+      -c 'set clipboard=unnamedplus' \
+      -c 'highlight Normal guibg=NONE ctermbg=NONE' \
+      "$@"
+  '';
 in {
   options.modules.editors.neovim.system.enable = mkEnableOption "Set EDITOR to neovim";
 
   config = mkIf cfg.enable {
+    environment.systemPackages = [editorScript];
+
     environment.variables = {
-      EDITOR = "nvim --clean -c 'set clipboard=unnamedplus' -c 'highlight Normal guibg=NONE ctermbg=NONE'";
+      EDITOR = "nvim-editor";
       MANPAGER = "nvim +Man!";
     };
   };
