72b6d6e3140952a88ad827f1962c2b36808ad884
Filipe Lima <datsfilipe.foss@proton.me>
2026-09-15 04:19:50 -0300

    fix: zip script calling itself

--- pkgs/scripts/default.nix
+++ pkgs/scripts/default.nix
@@ -1,6 +1,8 @@
 {
   unix-scripts,
   stdenvNoCC,
+  makeWrapper,
+  zip,
   ...
 }:
 stdenvNoCC.mkDerivation {
@@ -8,9 +10,15 @@ stdenvNoCC.mkDerivation {
   version = "unstable";
   src = unix-scripts;
 
+  nativeBuildInputs = [makeWrapper];
+
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/libexec
     cp -r ./* $out/bin/
     rm -f $out/bin/LICENSE $out/bin/README.md $out/bin/shared-clipboard
+
+    mv $out/bin/zip $out/libexec/zip
+    makeWrapper $out/libexec/zip $out/bin/zip \
+      --prefix PATH : ${zip}/bin
   '';
 }
